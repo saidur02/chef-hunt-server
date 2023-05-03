28ef5eecab5861685404a41feb9b39aa7dff942c
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-05-03 23:38:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+const chef = require('./data.json')
+
+app.use(cors());
+
+
+app.get('/',(req, res) =>{
+    res.send('dragon is Running')
+})
+
+
+
+app.get('/chef',(req,res) =>{
+    res.send(chef)
+})
+
+app.get('/chef/:id',(req,res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const selectedChef = chef.map(c =>c._id === id)
+    res.send(selectedChef)
+})
+
+
+app.listen(port,()=>{
+    console.log(`Dragon is running on port : ${port}`)
+})
