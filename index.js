4f66819ea4e047e9684374f74ecaf5cddc968a58
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-05-05 21:54:58 +0600

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/chef',(req,res) =>{
 app.get('/chef/:id',(req,res) =>{
     const id = req.params.id;
     console.log(id)
-    const selectedChef = chef.map(c =>c._id === id)
+    const selectedChef = chef.find(c =>c._id === id)
     res.send(selectedChef)
 })
 
